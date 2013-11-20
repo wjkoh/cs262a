b08435c198dc067fa7654df682624a24a6d49f87
Woojong Koh <wjngkoh@gmail.com>
2013-11-20 01:53:33 -0800

    Fix log file paths

--- logvis/__main__.py
+++ logvis/__main__.py
@@ -2,9 +2,9 @@
 from parser import LogParser
 
 if __name__ == '__main__':
-    error_fname = '../data/training_slave.aion.sarietta.log.ERROR.20131008-233011.10204'
-    warning_fname = '../data/training_slave.aion.sarietta.log.WARNING.20131008-233011.10204'
-    info_fname = '../data/training_slave.aion.sarietta.log.INFO.20131008-232039.10206'
+    error_fname = 'data/training_slave.aion.sarietta.log.ERROR.20131008-233011.10204'
+    warning_fname = 'data/training_slave.aion.sarietta.log.WARNING.20131008-233011.10204'
+    info_fname = 'data/training_slave.aion.sarietta.log.INFO.20131008-232039.10206'
 
     parser = LogParser()
     parser.parse_file(error_fname)
