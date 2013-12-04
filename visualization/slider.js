837717d25e5c079c9c07d06c2d35e33aa9e2f7d5
Armin Samii <samii@eecs.berkeley.edu>
2013-12-03 22:49:39 -0800

    Added slider functionality + icky message passing

--- visualization/slider.js
+++ visualization/slider.js
@@ -1,40 +1,51 @@
 // Each time parameters are updated, call this function
+// to update the displayed parameters
 function updateParams() {
-  // $( "#nodetext" ).val( "$" + $( "#timerange-slider" ).slider( "values", 0 ));
   $( "#slider > input" ).each(function(i, val) {
     // read initial values from markup and remove that
     $( this ).val($($( "#slider > span" ).get(i)).slider("value"))
   })
 }
 
-$(function() {
+// Sets up time window sliders
+function createTimeSlider() {
+  minTime = parseInt($( "#minTime" ).val(), 10);
+  maxTime = parseInt($( "#maxTime" ).val(), 10);
   $( "#timerange-slider" ).slider({
     range: true,
-    min: 0,
-    max: 500,
-    values: [ 75, 300 ],
+    min: minTime,
+    max: maxTime,
+    values: [ minTime, maxTime ],
     slide: function( event, ui ) {
-      $( "#timerange" ).val(ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+      $( "#timerange" ).val(ui.values[ 0 ] + " - " + ui.values[ 1 ]);
     }
   });
   $( "#timerange" ).val($( "#timerange-slider" ).slider( "values", 0 ) +
     " - " + $( "#timerange-slider" ).slider( "values", 1 ) );
-});
+}
 
-$(function() {
-  $( "#slider > span" ).each(function() {
+// Sets up num nodes / messages sliders
+function createGraphSlider() {
+  var maxes = $( "#maxSliders" ).val().split(' ');
+  $( "#slider > span" ).each(function(i) {
     // read initial values from markup and remove that
     var value = parseInt( $( this ).text(), 10 );
     $( this ).empty().slider({
       value: value,
       orientation: "horizontal",
       min: 1,
-      max: 20,
-      range: "min",
+      max: parseInt(maxes[i], 10),
       animate: true,
       step: 1,
-      slide: updateParams
+      slide: function( event, ui ) {
+        $($( "#slider > input" ).get(i)).val( ui.value );
+      }
     });
   });
+}
+
+function createToolbox() {
+  createTimeSlider();
+  createGraphSlider();
   updateParams();
-});
+}
